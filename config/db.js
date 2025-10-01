ac8d348a9cd34d09fb332001f963a00bcc35704d
TheoSudo <theoslaoq@gmail.com>
2025-10-01 15:20:17 -0300

    Mudanças

--- config/db.js
+++
@@ -1,6 +0,0 @@
-import Sequelize from 'sequelize';
-const sequelize = new Sequelize('restaurante', 'root', 'Theo0220!', {
-   host: "localhost",
-   dialect: 'mysql'
-});
-export { Sequelize, sequelize };
